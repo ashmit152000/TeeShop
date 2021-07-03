3d04708c6e2ad52dbef5e08bd2e5eb2502c38dbb
Ashmit Pathak <ashmitpathak@192.168.1.10>
2021-07-03 12:55:43 +0530

    added the contact us tab

--- lib/screens/contact_us.dart
+++ lib/screens/contact_us.dart
@@ -12,33 +12,33 @@ class ContactUs extends StatelessWidget {
         ),
         body: SingleChildScrollView(
           child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  Card(
-                    child: Column(children: [
-                      Image.asset('assets/images/3A.png'),
-                      Padding(padding: EdgeInsets.all(20)),
-                      Padding(
-                          padding: EdgeInsets.all(5),
-                          child: Text(
-                            'THE THREE MUSKETEERS ',
-                            style: TextStyle(
-                                fontSize:
-                                    MediaQuery.of(context).size.width / 10,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.purple,
-                                fontFamily: 'Jomhuria'),
-                          ))
-                    ]),
-                    elevation: 8,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text('This is the key'),
-                ],
-              )),
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Card(
+                  child: Column(children: [
+                    Image.asset('assets/images/3A.png'),
+                    Padding(padding: EdgeInsets.all(20)),
+                    Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Text(
+                          'THE THREE MUSKETEERS ',
+                          style: TextStyle(
+                              fontSize: MediaQuery.of(context).size.width / 10,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purple,
+                              fontFamily: 'Jomhuria'),
+                        ))
+                  ]),
+                  elevation: 8,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text('This is the key'),
+              ],
+            ),
+          ),
         ),
       ),
     );
