f09645cad600c00793192e3116796c70151b3141
Ashmit Pathak <ashmitpathak@192.168.1.7>
2021-07-21 21:14:41 +0530

    changes done

--- lib/screens/customize_screen.dart
+++ lib/screens/customize_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'dart:math';
 
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:image_cropper/image_cropper.dart';
